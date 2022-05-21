c8e63191c996c4bfc83d18f57e4a67bc8902a551
Eric Cheng <ratherman5796@gmail.com>
2022-05-21 11:09:17 +0800

    comment

--- toys/webapp01/main.go
+++ toys/webapp01/main.go
@@ -12,19 +12,25 @@ type Welcome struct {
 	Time string
 }
 
+// MVC 架構
 func main() {
 	// := means that Golang guess which type of varialbe
 	welcome := Welcome{"Sale Begins Now", time.Now().Format(time.Stamp)}
 	template := template.Must(template.ParseFiles("template/template.html"))
 
+	// bind 到 static，route 到一個網頁
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if sale := r.FormValue("sale"); sale != "" {
 			welcome.Sale = sale
 		}
+
+		// 使用 template.html
 		if err := template.ExecuteTemplate(w, "template.html", welcome); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	// 建立
 	fmt.Println(http.ListenAndServe(":8000", nil))
 }
